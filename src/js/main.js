eb711f0bd73f8498b5f7a9c2a3e548a6b3c9d77e
Mateusz <mkaminski.contact@gmail.com>
2023-04-07 10:38:25 +0200

    created desktop version of nav + navitem hover effects

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,20 @@
 const burgerBtn = document.querySelector('.burger-btn')
 const navItems = document.querySelector('.navbar__items')
+const navItem = document.querySelectorAll('.navbar__item')
+
 
 const handleNav = () => {
     navItems.classList.toggle('active')
     document.body.classList.toggle('nav-open')
 }
+const closeNav = () => {
+	navItems.classList.remove('active')
+	document.body.classList.remove('nav-open')
+}
+
+
 
 burgerBtn.addEventListener('click', handleNav)
+navItem.forEach(item => {
+    item.addEventListener('click', closeNav)
+})
