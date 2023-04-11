ae6710d1db0ee40a7f565d89625f47a95d180aee
Mateusz <mkaminski.contact@gmail.com>
2023-04-11 13:14:49 +0200

    added about section + accordion

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,33 @@ const navItems = document.querySelector('.navbar__items')
 const navItem = document.querySelectorAll('.navbar__item')
 const burgerInner = document.querySelector('.burger-btn__inner')
 
+const accordion = document.querySelector('.accordion');
+const accordionBtns= document.querySelectorAll('.accordion__btn');
+
+function openAccordionItems() {
+    if (this.nextElementSibling.classList.contains('accordion--active')) {
+        this.nextElementSibling.classList.remove('accordion--active');
+    } else {
+        closeAccordionItems();
+        this.nextElementSibling.classList.toggle('accordion--active');
+    }
+}
+
+const closeAccordionItems = () => {
+    const allActiveItems = document.querySelectorAll('.accordion__info');
+    allActiveItems.forEach(item => item.classList.remove('accordion--active'))
+}
+const clickOutsideAccordion = e => {
+	if (
+		e.target.classList.contains("accordion__btn") ||
+		e.target.classList.contains("accordion__info") ||
+		e.target.classList.contains("accordion__text")
+	)
+		return;
+
+	closeAccordionItems();
+};
+
 
 const handleNav = () => {
     navItems.classList.toggle('active')
@@ -20,3 +47,6 @@ burgerBtn.addEventListener('click', handleNav)
 navItem.forEach(item => {
     item.addEventListener('click', closeNav)
 })
+
+accordionBtns.forEach(btn => btn.addEventListener("click", openAccordionItems));
+window.addEventListener("click", clickOutsideAccordion);
