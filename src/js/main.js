a67774aba080414d6119da8856a947d215810e1c
Mateusz <mkaminski.contact@gmail.com>
2023-04-11 10:54:58 +0200

    completed nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
 const burgerBtn = document.querySelector('.burger-btn')
 const navItems = document.querySelector('.navbar__items')
 const navItem = document.querySelectorAll('.navbar__item')
+const burgerInner = document.querySelector('.burger-btn__inner')
 
 
 const handleNav = () => {
     navItems.classList.toggle('active')
     document.body.classList.toggle('nav-open')
+
 }
 const closeNav = () => {
 	navItems.classList.remove('active')
